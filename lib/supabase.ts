e9f82b41f0a3e291db48ae4104cdbf2722773d1f
Esteban <91676362+estebanbaigts@users.noreply.github.com>
2025-03-07 12:52:15 +0100

    add a blank line for improved readability in supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,6 +2,7 @@ import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 import * as SecureStore from 'expo-secure-store';
 
+
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
     return SecureStore.getItemAsync(key);
